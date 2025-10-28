c586bbb4cdc9fac32dac91ed95e2e7d182e2cbf2
Prince <princechauhan112004@gmail.com>
2025-10-28 19:07:49 +0530

    chat page setup

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useState, useRef } from 'react';
+import {LogOutIcon, VolumeOffIcon,Volume2Icon} from "lucide-react"
+import {useAuthStore} from "../store/useAuthStore";
+import {useChatStore} from "../store/useChatStore";
 
 const ProfileHeader = () => {
+  const {logout,authUser,updateProfile}= useAuthStore();
+  const {isSoundEnabled,toggleSound} =useChatStore();
+  const [selectedImg,setSelectedImg]=useState(null);
+  const fileInputRef=useRef(null);
+
+  const handleImageUpload=(e)=>{
+
+  }
+
   return (
-    <div>ProfileHeader</div>
+    <div className='p-6 border-b border-slate-700/50'>
+      <div className=' flex flex-col items-center justify-between'>
+       <div  className='flex items-center gap-3'>
+          {/* Avatar */}
+            <div className='avatar avatar-online '>
+
+              <button className='size-14 rounded-full overflow-hidden relative group' onClick={()=>fileInputRef.current.click()}>
+                <img src={selectedImg || authUser.profilePic || "/avatar.png"} alt="User image"  className='size-full object-cover'/>
+
+                 <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity cursor-pointer duration-400">
+                  <span className="text-white text-xs">Change</span>
+
+                </div>
+              </button>
+
+              <input type="file"
+              accept='image/*'
+              ref={fileInputRef}
+              onChange={handleImageUpload}
+              className='hidden'
+              />
+            </div>
+       </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,25 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import daisyui from "daisyui";
+
+
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss({
-      content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-      theme: {
-        extend: {
-          animation: {
-            border: "border 4s linear infinite",
-          },
-          keyframes: {
-            border: {
-              to: { "--border-angle": "360deg" },
-            },
-          },
-        },
-      },
-      plugins: [daisyui],
-    }),
+    tailwindcss(),
   ],
 });
